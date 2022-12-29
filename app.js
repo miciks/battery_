9c77a696e07c560ef404264f753abbd2264c4924
miciks <micikahmetali@gmail.com>
2022-12-29 20:12:17 +0300

    app.js oluşturuldu.

---
+++ app.js
@@ -0,0 +1,8 @@
+let one = document.querySelector("#batteryOne")
+let two = document.querySelector("#batteryTwo")
+let three = document.querySelector("#batteryThree")
+let show = document.getElementById("batteryShow")
+
+let raise = document.getElementById("raise").addEventListener('click', fulle)
+let minus = document.getElementById("minus").addEventListener('click', boşalt)
+
